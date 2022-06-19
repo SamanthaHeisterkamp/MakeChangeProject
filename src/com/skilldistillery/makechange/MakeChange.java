6a45f76fff050afec089bb1185f50ba56e370d6a
Samantha Heisterkamp <samanthapaige912@yahoo.com>
2022-06-19 13:20:31 -0600

    its working, but not fancy enough

--- src/com/skilldistillery/makechange/MakeChange.java
+++ src/com/skilldistillery/makechange/MakeChange.java
@@ -24,33 +24,96 @@ public class MakeChange {
 		// tendered by the customer.
 		System.out.println("How much did the customer pay with?");
 		customerPay = sc.nextDouble();
+		sc.close();
 
-		if (itemPrice < customerPay)
+		if (itemPrice < customerPay) {
 			custChange = customerPay - itemPrice;
-		System.out.println("Your change is: $" + custChange);
-		cashRegister(custChange);
-		System.out.println("Your exact change is ");
-		if (itemPrice == customerPay)
-			exactAmount();
-		if (itemPrice > customerPay)
+			System.out.println("Your change is: ");
+			cashRegister(custChange);
+		}
+
+		if (itemPrice > customerPay) {
 			System.out.println("Please provide the appropriate amount.");
-		owe = itemPrice - customerPay;
-		System.out.println("You owe " + owe);
+			owe = itemPrice - customerPay;
+			customerOwe(owe);
+		}
+
+		else if (itemPrice == customerPay) {
+			exactAmount();
+		}
 
 	}
 
 	public static void cashRegister(double custChange) {
-		System.out.println("you are in the register" + custChange);
 
-	}
+		if (custChange >= 100) {
+			int changeHundred = (int) (custChange / 100);
+			System.out.println(changeHundred + " Hundred dollar bills.");
+		}
+			custChange = custChange % 100;
+		if (custChange >= 50) {
+			int changeFifty = (int) (custChange / 50);
+			System.out.println(changeFifty + " Fifty dollar bills.");
+		}
+			custChange = custChange % 50;
+			if (custChange >= 20) {
+				int changeTwenty = (int) (custChange / 20);
+				System.out.println(changeTwenty + " Twenty dollar bills.");
+		}
+			custChange = custChange % 20;
+			if (custChange >= 10) {
+				int changeTen = (int) (custChange / 10);
+				System.out.println(changeTen + " Ten dollar bills.");
+			}
+			custChange = custChange % 10;
+			if (custChange >= 5) {
+				int changeFive = (int) (custChange / 5);
+				System.out.println(changeFive + " Five dollar bills.");
+			}
+			custChange = custChange % 5;
+			if (custChange >= 1) {
+				int changeOne = (int) (custChange / 1);
+				System.out.println(changeOne + " One dollar bills.");
+			}
+			custChange = custChange % 1;
+			if (custChange >= .25) {
+				int changeQuarter = (int)(custChange / .25);
+				System.out.println(changeQuarter + " Quarters.");
+			}
+			custChange = (custChange % .25);
+			if (custChange >= .10) {
+				int changeDime = (int) (custChange / .10);
+				System.out.println(changeDime + " Dimes");
+				custChange = custChange % .10;
+			}
+			if (custChange >= .05) {
+				int changeNickel = (int) (custChange / .05);
+				System.out.println(changeNickel + " Nickels.");
+				custChange = custChange % .05;
+			}
+			if (custChange >= .01) {
+				custChange += 0.005;
+				int changePenny = (int) (custChange / .01);
+				System.out.println(changePenny + " Pennies.");
+				custChange = custChange % .01;
+			}
+		}
+
+
+	
+
 	public static void exactAmount() {
-	System.out.println("Thank you for providing the exact amount!");
+		System.out.println("Thank you for providing the exact amount!");
+	}
 
+	public static void customerOwe(double y) {
+		System.err.print("You owe " + y);
+		
+	}
 //		Display an appropriate message if the customer provided
 //		too little money or the exact amount.
 
 	// If the amount tendered is more than the cost of the
 	// item, display the number of bills and coins that
 	// should be given to the customer.
-}
 }
